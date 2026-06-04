6e43952e9104500c1cd65fbcd7882293620957bc
NaveenaEB <Naveena.EB@lancesoft.in>
2026-06-04 15:32:05 +0530

    new changes

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -7,6 +7,9 @@ def register_user(db: Session, user: user_schema.UserCreate):
     # Here you can add logic like checking if email exists
     return user_repository.create_user(db, user)
 
+def get_all_users(db: Session, skip: int = 0, limit: int = 100):
+    return user_repository.get_users(db, skip=skip, limit=limit)
+
 def get_profile(db: Session, user_id: int):
     return user_repository.get_user(db, user_id)
 
